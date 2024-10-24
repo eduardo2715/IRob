1be7a8e13332f5c15d83d358ec7def7281e6973d
Eduardo Guerra <eduadro.a.c.guerra@tecnico.ulisboa.pt>
2024-10-25 00:31:12 +0100

    aa

--- P1/turtlebot3_datasets/scripts/download_dataset.sh
+++ P1/turtlebot3_datasets/scripts/download_dataset.sh
@@ -2,8 +2,8 @@
 path_to_pkg=`rospack find turtlebot3_datasets`/data
 
 # Download and extract the first tar.gz (existing dataset)
-gdown -O $path_to_pkg/ir_labs.tar.gz https://drive.google.com/uc?id=1-QdzXIoh0ltdDNGuffLqzsPZLnLwSjAj && tar -xvf $path_to_pkg/ir_labs.tar.gz -C $path_to_pkg
+# gdown -O $path_to_pkg/ir_labs.tar.gz https://drive.google.com/uc?id=1-QdzXIoh0ltdDNGuffLqzsPZLnLwSjAj && tar -xvf $path_to_pkg/ir_labs.tar.gz -C $path_to_pkg
 
 # Download and extract the new tar.gz (your new rosbag)
-gdown -O $path_to_pkg/real_robot_rosbag.tar.gz https://drive.google.com/uc?id=1R0MUgLJI4AMkzw7CrRbe7_Xnjo3CPW0L && tar -xvf $path_to_pkg/real_robot_rosbag.tar.gz -C $path_to_pkg
+gdown -O $path_to_pkg/real_robot_rosbag.tar.gz https://drive.google.com/uc?id=1sDR_OelXr9GHtt_8MoH_BPHkH27IakAl && tar -xvf $path_to_pkg/real_robot_rosbag.tar.gz -C $path_to_pkg
 
